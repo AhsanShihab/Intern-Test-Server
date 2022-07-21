13668fef0498c8cecb611be3337f902fcf0cc75f
Nazmul Ahsan <ahsan.shihab2@gmail.com>
2022-07-21 20:30:34 +0600

    fix 401 response

--- dependencies.py
+++ dependencies.py
@@ -6,7 +6,7 @@ from authentication.security_config import decode_token
 
 
 class JWTBearer(HTTPBearer):
-    def __init__(self, auto_error: bool = True):
+    def __init__(self, auto_error: bool = False):
         super(JWTBearer, self).__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request):
